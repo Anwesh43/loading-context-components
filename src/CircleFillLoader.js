780fb13d268fa77b922a16f775012e040aeb4ce3
Anwesh43 <anweshthecool0@gmail.com>
2020-12-18 05:18:11 +0530

    adding Loader components

---
+++ src/CircleFillLoader.js
@@ -0,0 +1,20 @@
+import LoadingContext from './LoadingContext'
+import React, {useContext} from 'react'
+const CircleFillLoader = ({w, h}) => {
+    const sf = useContext(LoadingContext)
+    const size = Math.min(w, h) * 0.2 * sf 
+    const style = {
+        width : `${size}px`,
+        height : `${size}px`,
+        position: 'absolute', 
+        left : `${w / 2}px`,
+        top : `${2 * h / 3}px`,
+        borderRadius: '50%'
+    }
+    return (
+        <div style = {style}>
+        </div>
+    )
+}
+
+export default CircleFillLoader 
---
+++ src/LineLoader.js
@@ -0,0 +1,19 @@
+import LoadingContext from './LoadingContext'
+import React, {useContext} from 'react'
+const LineLoader = ({color, w, h}) => {
+    const sf = useContext(LoadingContext)
+    const style = {
+        background : color,
+        position : 'absolute',
+        width : `${w * sf}px`, 
+        top : `${2 * h / 7}px`,
+        height : `${h / 50}px`, 
+        left : `${0}px`
+    }
+    return (
+      <div style = {style}>
+      </div>
+    )
+}
+
+export default LineLoader 
